12a54478a921c269ad1e3ba608fc1cf121aaf7f4
Zach Samuels <wzsamuels@gmail.com>
2023-11-24 13:57:09 -0500

    Add custom theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        themeBlue: "rgb(29 78 216)"
+      }
+    },
   },
   plugins: [],
 }
